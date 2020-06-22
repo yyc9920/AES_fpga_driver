948175c157c0aaef93a815b91a3788c0b3419e7d
yechanyun <yyc837021@gmail.com>
2020-06-22 16:54:09 +0900

    add c-header

---
+++ jupyter notebook/test.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('images/money.jpg',0)
+
+img = cv2.resize(img, dsize=(0, 0), fx=0.2, fy=0.2, interpolation=cv2.INTER_AREA)
+
+get = []
+c = 0
+for a in img:
+    for b in a:
+        if b > 100: c=1
+        else: c=0
+        get.append(c)
+
+get_str = str(get)
+get_str = get_str.replace("[", "{")
+get_str = get_str.replace("]","};")
+
+print(get_str)
