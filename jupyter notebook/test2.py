b97a062980e9af7fc4ad399906f7bdb44ac292ce
yechanyun <yyc837021@gmail.com>
2020-06-22 18:42:24 +0900

    please3

--- jupyter notebook/test2.py
+++ jupyter notebook/test2.py
@@ -4,14 +4,16 @@ from matplotlib import pyplot as plt
 
 img = cv2.imread('images/locker.jpg',0)
 
+##img = cv2.threshold(img,127,255, cv2.THRESH_BINARY_INV)
+
 img = cv2.resize(img, dsize=(150, 50), interpolation=cv2.INTER_AREA)
 
 get = []
 c = 0
 for a in img:
     for b in a:
-        if b > 100: c=1
-        else: c=0
+        if b > 100: c=0
+        else: c=1
         get.append(c)
 
 get_str = str(get)
